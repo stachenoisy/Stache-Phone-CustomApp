dfba0e2f8c2301c75c19d95ae8419da71480db2f
Stache Noisy <66780609+stachenoisy@users.noreply.github.com>
2024-01-28 16:51:44 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+author 'Stache'
+description 'Stache Phone Custom App'
+version '1.0.0'
+game 'gta5'
+
+-- ui_page{
+--     'ui/index.html'
+-- } 
+
+files {
+    'ui/index.html',
+}
